31f3ac68f8d2fd230f299cc73f6147e20c0736b0
Kjartan <kjartanreinarsson@gmail.com>
2022-04-04 22:28:37 -0700

    inital file setup

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Box from '@mui/material/Box';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Box> Poop</Box>
+    </div>
+  );
+}
+
+export default App;
