434a97575375b6e5a576aa7e7712e1bf9da7f876
Sam Warren <samwarren4@gmail.com>
2022-04-17 19:36:11 -0700

    shorten hot reload interval

--- DMTools/app/src/utils/HotReloadSetup.js
+++ DMTools/app/src/utils/HotReloadSetup.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local') 
     if (webPackConfigFileText.includes('performance: false,')) {
       webPackConfigFileText = webPackConfigFileText.replace(
         'performance: false,',
-        "performance: false,\n\t\twatchOptions: { aggregateTimeout: 200, poll: 1000, ignored: '**/node_modules', },"
+        "performance: false,\n\t\twatchOptions: { aggregateTimeout: 200, poll: 500, ignored: '**/node_modules', },"
       );
       fs.writeFileSync(webPackConfigFile, webPackConfigFileText, 'utf8');
     } else {
