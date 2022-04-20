9f7f9a868db2cb15fce6651b83f44d9e13aca62d
Kjartan <kjartanreinarsson@gmail.com>
2022-04-20 01:00:22 -0700

    Header, navbar improvments

--- DMTools/app/src/components/layout/Header.tsx
+++ DMTools/app/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Avatar, Box, Theme, Toolbar } from '@mui/material';
+import { AppBar, Avatar, Box, Divider, IconButton, Menu, MenuItem, Theme, Toolbar, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -6,22 +6,96 @@ import { Link } from 'react-router-dom';
 const useStyles = makeStyles((theme: Theme) => ({
   head: {
     height: '100%'
+  },
+  mainNavToolbar: {
+    '& a': {
+      display: 'block',
+      padding: 2,
+      color: 'inherit',
+      fontSize: '1.6rem',
+      textDecoration: 'none'
+    },
+    '& a:hover': {
+      textDecoration: 'underline'
+    },
+    '& a:first-child': {
+      marginLeft: -2
+    }
+  },
+  menuDecorator:{
+    color:'black', 
+    textDecoration: 'none' 
   }
 }));
 
 const Header: React.FC = () => {
   const classes = useStyles();
 
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <Box className={classes.head}>
       <AppBar position="sticky" style={{ boxShadow: 'inherit' }}>
-        <Toolbar variant="dense" role="navigation" >
-          <Link to="/">
-            <Avatar alt="Logo" src="/assets/dnd-icon-13.jpg" sx={{ width: 80, height: 80 }} />
-          </Link>
+        <Toolbar variant="regular" className={classes.mainNavToolbar} role="navigation">
+          <Typography variant="h4" noWrap component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            DM_Tools
+          </Typography>
           <Link to="/">Home</Link>
+          <Box px={1}>
+            <Divider sx={{ color: 'white' }} orientation="vertical" />
+          </Box>
           <Link to="/initiative-tracker">Initiative Tracker</Link>
-          <Link to="/reputation-tracker">Reputation Tracker</Link>
+          <Box px={1}>
+            <Divider orientation="vertical" />
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link to="/reputation-tracker">Reputation Tracker</Link>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton onClick={handleOpenNavMenu} sx={{ p: 0 }}>
+              <Avatar alt="d20" src="/assets/dnd-icon-13.jpg" />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px', color: 'white'}}
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}>
+              <Link to="/" className={classes.menuDecorator} >
+                <MenuItem key="home" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">Home</Typography>
+                </MenuItem>
+              </Link>
+              <Link to="/initiative-tracker" className={classes.menuDecorator} >
+                <MenuItem key="initiative-tracker" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">Initiative Tracker</Typography>
+                </MenuItem>
+              </Link>
+              <Link to="/reputation-tracker" className={classes.menuDecorator} >
+                <MenuItem key="reputation-tracker" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">Reputation Tracker</Typography>
+                </MenuItem>
+              </Link>
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
--- DMTools/app/src/layouts/DmHome.tsx
+++ DMTools/app/src/layouts/DmHome.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import React from 'react';
 import Footer from '../components/layout/Footer';
 import Header from '../components/layout/Header';
@@ -7,11 +7,13 @@ const DmHome: React.FC = (props) => {
   return (
     <Box>
       <Header />
-      <Box component="main" flex="1 1 auto" py={4}>
-        {React.Children.map(props.children, (child: any) => {
-          return React.cloneElement(child);
-        })}
-      </Box>
+      <Container maxWidth="xl">
+        <Box component="main" flex="1 1 auto" py={4}>
+          {React.Children.map(props.children, (child: any) => {
+            return React.cloneElement(child);
+          })}
+        </Box>
+      </Container>
       <Footer />
     </Box>
   );
